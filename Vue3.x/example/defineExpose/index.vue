54af7cae5e916a6d34e8a2678d60b4a2fd7f12fe
Kerwin <if5400@gmail.com>
2022-07-04 14:29:53 +0800

    Create index.vue

---
+++ Vue3.x/example/defineExpose/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import GroupSet from './components/GroupSet.vue'
+import { ref, computed } from 'vue'
+import type { IGroupSet } from './components/utile_type';
+let $groupSet = ref<IGroupSet|null>(null)
+let shoppingCar = computed(() => {
+  return $groupSet
+})
+</script>
+
+<template lang="pug">
+div
+  GroupSet(ref="$groupSet")
+  //- 可響應數據
+  div {{ shoppingCar?.value?.inputValue }}
+</template>
